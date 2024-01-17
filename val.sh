4f2ccd9aed50ad044f89507241350bd082236202
fangbufang <44249380+fangbufang@users.noreply.github.com>
2024-01-17 17:48:53 +0800

    Add files via upload

---
+++ val.sh
@@ -0,0 +1,18 @@
+CUDA_VISIBLE_DEVICES=0,1,2 mmf_run dataset=sal_textvqa \
+  model=sal \
+  config=projects/sal/configs/textvqa/adam.yaml \
+  env.save_dir=./save/sal_train\
+  run_type=val \
+  training.seed=1\
+  training.num_workers=2\
+  training.batch_size=36\
+  training.max_updates=24000\
+  training.evaluation_interval=1000\
+  training.checkpoint_interval=1000\
+  checkpoint.max_to_keep=5\
+  optimizer.params.lr=2e-4\
+  model_config.sal.mmt.dropout_rate=0.1\
+  checkpoint.resume_file=./save/textvqa/base/best.ckpt
+
+
+
